489d03effd4bc10d52f0b2f9c181a8190dbf141c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-10 23:56:14 +0000

    Deploy cdc.hitcon.org to github.com/hacksintaiwan/CCIP-HDP.git:gh-pages

---
+++ precache-manifest.b1847961cfb97403404d4397924665e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a982aed2c97c3c171a",
+    "url": "/css/app.3f02ad13.css"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "c23250d306ac8240512a748d7e9e3eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a982aed2c97c3c171a",
+    "url": "/js/app.403bdacd.js"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/js/chunk-vendors.db9426a7.js"
+  },
+  {
+    "revision": "4ac946e97bd347f4ff35",
+    "url": "/js/declaration.b9b8cecf.js"
+  },
+  {
+    "revision": "887e96f796ae5c14533c",
+    "url": "/js/fill.ef446c54.js"
+  },
+  {
+    "revision": "c930cc70047941b7d0f6",
+    "url": "/js/status.e4e6ffb2.js"
+  },
+  {
+    "revision": "28bf2ab99af2d0770d51a57d31b2f113",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
